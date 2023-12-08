8304628af5473fcae13820c1c3b1f655f6d0dd58
Pixelpip <athulkamathg5@gmail.com>
2023-12-08 18:44:56 +0530

    1

--- new_proj/blog/views.py
+++ new_proj/blog/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts=[
+    {
+        'author':'Pada',
+        'title':'Blog post 1',
+        'content':'First Post',
+        'date_posted':'August 28 2023'
+    },
+    {
+        'author':'Dada',
+        'title':'Blog post 2',
+        'content':'2nd Post',
+        'date_posted':'August 30 2023'
+    },
+    
+]
 
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
