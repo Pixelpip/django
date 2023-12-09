5df0a2edd0b151f89a817331b729095e06a56a7f
Pixelpip <athulkamathg5@gmail.com>
2023-12-10 00:21:28 +0530

    1

--- new_proj/blog/models.py
+++ new_proj/blog/models.py
@@ -5,5 +5,6 @@ from django.utils import timezone
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
+    date_posted=models.DateTimeField(auto_now_add=True)
+    
 
